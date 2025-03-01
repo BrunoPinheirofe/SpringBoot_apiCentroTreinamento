65e656f3ea451ea20e9cded6f753f15224e1347a
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2025-03-01 09:20:21 -0300

    iniciando projeto

---
+++ src/main/java/com/syntaxsquad/ltd/apiCentroTreinamento/ApiCentroTreinamentoApplication.java
@@ -0,0 +1,13 @@
+package com.syntaxsquad.ltd.apiCentroTreinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCentroTreinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCentroTreinamentoApplication.class, args);
+	}
+
+}
