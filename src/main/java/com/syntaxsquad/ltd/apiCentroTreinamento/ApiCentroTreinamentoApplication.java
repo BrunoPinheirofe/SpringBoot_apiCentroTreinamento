d6a90117b4c229abcb751ad22860e8ee9bf934e4
6abrie1 <gabrieljv2019@gmail.com>
2025-03-16 23:16:23 -0300

    Tem fazer as variaveis de ambiente ( dba,smtp,token,mercado pago )

--- src/main/java/com/syntaxsquad/ltd/apiCentroTreinamento/ApiCentroTreinamentoApplication.java
+++ src/main/java/com/syntaxsquad/ltd/apiCentroTreinamento/ApiCentroTreinamentoApplication.java
@@ -19,6 +19,11 @@ public class ApiCentroTreinamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCentroTreinamentoApplication.class, args);
+		System.out.println(" GGGGG    AAAAA   BBBBB   RRRRR    III  EEEEE  L      ");
+        System.out.println("G         A   A   B    B  R   R     I   E       L      ");
+        System.out.println("G  GG     AAAAA   BBBBB   RRRRR     I   EEEE    L      ");
+        System.out.println("G   G     A   A   B    B  R  R      I   E       L      ");
+        System.out.println(" GGGG     A   A   BBBBB   R   R    III  EEEEE   LLLLL  ");
 		
 	}
 
