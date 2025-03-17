acf83aa609b05449fbe7a32b42839ce9a0790f6a
6abrie1 <gabrieljv2019@gmail.com>
2025-03-16 23:20:38 -0300

    Tela inicial

--- src/main/java/com/syntaxsquad/ltd/apiCentroTreinamento/ApiCentroTreinamentoApplication.java
+++ src/main/java/com/syntaxsquad/ltd/apiCentroTreinamento/ApiCentroTreinamentoApplication.java
@@ -19,11 +19,27 @@ public class ApiCentroTreinamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCentroTreinamentoApplication.class, args);
+		System.out.println("\n");
+		System.out.println("\n");
+
 		System.out.println(" GGGGG    AAAAA   BBBBB   RRRRR    III  EEEEE  L      ");
         System.out.println("G         A   A   B    B  R   R     I   E       L      ");
         System.out.println("G  GG     AAAAA   BBBBB   RRRRR     I   EEEE    L      ");
         System.out.println("G   G     A   A   B    B  R  R      I   E       L      ");
         System.out.println(" GGGG     A   A   BBBBB   R   R    III  EEEEE   LLLLL  ");
+
+		System.out.println("\n");
+		System.out.println("\n");
+
+
+		System.out.println("BBBBB   RRRRR    U   U   N   N   OOOOO  ");
+        System.out.println("B    B  R   R    U   U   NN  N  O     O ");
+        System.out.println("BBBBB   RRRRR    U   U   N N N  O     O ");
+        System.out.println("B    B  R  R     U   U   N  NN  O     O ");
+        System.out.println("BBBBB   R   R     UUU    N   N  OOOOO  ");
+
+		System.out.println("\n");
+		System.out.println("\n");
 		
 	}
 
